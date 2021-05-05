1521c349d28a0e817bdb521cbd3054785a663ea4
Frédéric Glorieux <frederic.glorieux@fictif.org>
2021-05-06 01:11:54 +0200

    Plus d’accueil

---
+++ hauy/index.php
@@ -0,0 +1,146 @@
+<?php
+
+// Soumission en post, lancer la transformation
+if (isset($_POST['post'])) {
+  include(dirname(dirname(__FILE__)).'/odette.php');
+  Odette::doPost();
+  exit;
+}
+?><!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Odette (odt ► TEI), Hauy</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&amp;display=swap" rel="stylesheet"/>
+    <style>
+* {
+  box-sizing: border-box;
+}
+header {
+  background: #ffffff;
+}
+body {
+  margin: 0;
+  background: #f7f7f7;
+  color: #26458a;
+  font-family: Lato, Helvetica, sans-serif;
+}
+footer,
+header {
+  text-align: center;
+}
+footer,
+header nav {
+  height: 90px;
+  background: #26458a;
+  color: #fff;
+  font-weight: bold;
+}
+
+header nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+
+footer a,
+header nav a {
+  color: #fff;
+  border: none;
+  text-decoration: none;
+}
+
+main,
+div.sep {
+  width: 67%;
+  margin: 0 auto;
+}
+div.sep {
+  text-align: center;
+  align-items: center;
+  -webkit-box-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90px;
+}
+div.sep > div {
+  display: inline-block;
+  margin: 0 10px;
+}
+div.sep:before,
+div.sep:after {
+    display: block;
+    content: "";
+    border-bottom: 0;
+    -webkit-box-flex: 1;
+    -ms-flex-positive: 1;
+    flex-grow: 1;
+    border-top: 1.9px solid #fff;
+}
+    </style>
+  </head>
+  <body>
+    <div id="center">
+      <header id="header">
+        <a href="https://obtic.sorbonne-universite.fr/"><img width="368px" src="obtic.svg"/></a>
+        <nav>
+        <a href="https://www.avh.asso.fr/">Association Valentin Haüy</a>
+        </nav>
+      </header>
+      <main>
+
+
+    <?php
+  if (isset($_REQUEST['format'])) $format=$_REQUEST['format'];
+  else $format="tei";
+    ?>
+        <form class="gris" enctype="multipart/form-data" method="POST" name="odt" action="index.php">
+          <input type="hidden" name="post" value="post"/>
+          <input type="hidden" name="model" value="<?php echo basename(dirname(__FILE__)) ?>"/>
+          <div style="margin: 50px 0 20px;">
+            <b>1. Fichier odt</b> :
+            <input type="file" size="70" name="odt" accept="application/vnd.oasis.opendocument.text"/><!-- ne sort pas ds chrome -->
+          </div>
+
+          <div style="margin: 20px 0 20px;">
+            <b>2. Format d'export</b> :
+                <label title="TEI"><input name="format" type="radio" value="tei" <?php if($format == 'tei') echo ' checked="checked"'; ?>/> tei</label>
+              — <label title="OpenDocument Text xml"><input name="format" type="radio" value="html" <?php if($format == 'html') echo ' checked="checked"'; ?>/> html</label>
+          </div>
+
+          <div style="margin: 20px 0 40px;">
+            <b>3. Résultat</b> :
+            <input type="submit" name="view"  value="Voir"/> ou
+              <input type="submit" name="download" onclick="this.form" value="Télécharger"/>
+          </div>
+        </form>
+          <?php
+          $odt = glob(dirname(__FILE__)."/*.odt");
+          $count = count($odt);
+          if ($count) {
+            if ($count > 1) $mess = "Exemples de documents";
+            else $mess = "Exemple de document";
+            echo "<b>".$mess."</b>\n";
+            echo "<ul>\n";
+            foreach ($odt as $file) {
+              $basename = basename($file);
+              echo "<li><a href=\"".$basename."\">".$basename."</a></li>\n";
+            }
+            echo "</ul>\n";
+          }
+           ?>
+          
+      </main>
+      <footer>
+        <div class="sep">
+          <div>
+            <a href="https://obtic.sorbonne-universite.fr/"><img align="middle" width="50px" src="otc.svg"/></a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -14,60 +14,22 @@ if (isset($_POST['post'])) {
     <title>Odette : Open Document Text &gt; XML/TEI</title>
   </head>
   <body>
-    <div id="center">
-      <header id="header">
-        <h1>
-          <a href="../">Développements</a>
-        </h1>
-        <a class="logo" href="//obvil.paris-sorbonne.fr/developpements/"><img class="logo" src="//svn.code.sf.net/p/obvil/code/theme/img/logo-obvil.png" alt="OBVIL"></a>
-      </header>
-      <div id="contenu">
 
     <h1>Odette, convertissez vos textes bureautiques (odt) en <a href="//www.tei-c.org/release/doc/tei-p5-doc/fr/html/REF-ELEMENTS.html">TEI</a></h1>
-    <p class="byline">par <a onmouseover="this.href='mailto'+'\x3A'+'frederic.glorieux'+'\x40'+'fictif.org'" href="#">Frédéric Glorieux</a></p>
-    <!--
+    
+    <p>Sur cette machine, plusieurs projets ont installé un modèle de document :</p>
     <ul>
-      <li>Parcourir : chercher un fichier OpenOffice odt sur votre poste</li>
-      <li>Voir : montrer le XML produit</li>
-      <li>Télécharger : enregistrer le produit sur son poste</li>
+        <?php
+    foreach(glob(dirname(__FILE__)."/*", GLOB_ONLYDIR) as $dir) {
+      $basename = basename($dir);
+      if ($basename[0] == '.' || $basename[0] == '_') continue;
+      echo "<li><a href=\"".$basename."/\">".$basename."</a></li>\n";
+    }
+     ?>
     </ul>
-    -->
+
+    <p>Pour plus d’explications : <a href="//resultats.hypotheses.org/267">Glorieux, 2015. <i>Le traitement de textes pour produire des documents structurés (XML/TEI)</i></a>.</p> <p>Renseignements, <a onmouseover="this.href='mailto'+'\x3A'+'frederic.glorieux'+'\x40'+'fictif.org'" href="#">Frédéric Glorieux</a>.</p>
 
 
-    <?php
-  if (isset($_REQUEST['format'])) $format=$_REQUEST['format'];
-  else $format="tei";
-  /*
-        — <label title="OpenDocument Text xml"><input name="format" type="radio" value="ngml" <?php if($format == 'ngml') echo ' checked="checked"'; ?>/> NGML </label>
-
-        — <label title="HTML (Diple)"><input name="format" type="radio" value="html" <?php if($format == 'html') echo ' checked="checked"'; ?>/> HTML </label>
-  */
-    ?>
-    <form enctype="multipart/form-data" method="POST" name="odt" action="index.php">
-      <input type="hidden" name="post" value="post"/>
-      <div style="margin: 50px 0 20px;">
-        <b>1. Fichier odt</b> :
-        <input type="file" size="70" name="odt" accept="application/vnd.oasis.opendocument.text"/><!-- ne sort pas ds chrome -->
-      </div>
-
-      <div style="margin: 20px 0 20px;">
-        <b>2. Format d'export</b> :
-            <label title="TEI"><input name="format" type="radio" value="tei" <?php if($format == 'tei') echo ' checked="checked"'; ?>/> tei</label>
-          — <label title="OpenDocument Text xml"><input name="format" type="radio" value="odtx" <?php if($format == 'odtx') echo ' checked="checked"'; ?>/> xml odt</label>
-          — <label title="OpenDocument Text xml"><input name="format" type="radio" value="html" <?php if($format == 'html') echo ' checked="checked"'; ?>/> html</label>
-          <!--
-          | <label title="Indiquer le mot clé d'un autre format">Autres formats <input name="local" size="10"/></label>
-         -->
-      </div>
-
-      <div style="margin: 20px 0 40px;">
-        <b>3. Résultat</b> :
-        <input type="submit" name="view"  value="Voir"/> ou
-          <input type="submit" name="download" onclick="this.form" value="Télécharger"/>
-      </div>
-    </form>
-    <p>Pour plus d’explications : <a href="//resultats.hypotheses.org/267">Glorieux, 2015. <i>Le traitement de textes pour produire des documents structurés (XML/TEI)</i></a>. N'hésitez pas à m’<a onmouseover="this.href='mailto'+'\x3A'+'frederic.glorieux'+'\x40'+'fictif.org'" href="#">envoyer</a> vos cas épineux.</p>
-      </div>
-    </div>
   </body>
 </html>
